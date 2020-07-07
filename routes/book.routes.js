1524ea12c3a723f6a2c485a32833b08b4894230a
rschpdr <pedro.resch@gmail.com>
2020-07-07 17:38:19 -0300

    Added rest of CRUD methods

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,23 +1,83 @@
 // ROUTES FILE NEEDS TO BE REQUIRED IN THE APP.JS IN ORDER NOT TO GIVE 404
 // APP NEEDS TO KNOW YOU CREATED A NEW ROUTE FILE, THAT'S THE ONLY WAY FOR IT TO KNOW WHICH ROUTES YOU WANT TO HIT
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // ********* require Book model in order to use it *********
-const Book = require('../models/Book.model');
+const Book = require("../models/Book.model");
+
+// Requisição HTTP
+// Header
+// Body - GET não tem body
+
+// CREATE
+router.post("/books/create", async (req, res) => {
+  // Acessar os dados no body da requisição
+  const data = req.body;
+  console.log("REQ BODY => ", data);
+
+  // Inserir esses dados no banco
+  try {
+    const result = await Book.create({ ...data, rate: Number(data.rate) });
+    console.log("RESULTADO DA INSERÇÃO => ", result);
+
+    // Retornar sucesso para o usuário
+    res.redirect("/books");
+  } catch (err) {
+    console.error(err);
+    throw new Error(err);
+  }
+});
+
+// UPDATE
+router.post("/books/update/:someBookId", async (req, res) => {
+  const { someBookId } = req.params;
+
+  const data = req.body;
+
+  try {
+    const result = await Book.updateOne({ _id: someBookId }, { $set: data });
+    console.log(result);
+
+    res.redirect(`/books/${someBookId}`);
+  } catch (err) {
+    throw new Error(err);
+  }
+
+  // Atualizar com os novos dados no banco
+  // Retornar sucesso ou erro para o usuário
+});
+
+// DELETE
+router.get("/books/delete/:someBookId", async (req, res) => {
+  // Decidir qual livro deletar
+  const { someBookId } = req.params;
+
+  // Deletar o livro do banco
+  try {
+    const result = await Book.deleteOne({ _id: someBookId });
+    console.log(result);
+
+    res.redirect("/books");
+  } catch (err) {
+    throw new Error(err);
+  }
+
+  // Retornar sucesso ou erro para o usuário
+});
 
 // ****************************************************************************************
 // GET route to display all the books
 // ****************************************************************************************
 
-router.get('/books', (req, res) => {
+router.get("/books", (req, res) => {
   Book.find()
-    .then(allTheBooksFromDB => {
+    .then((allTheBooksFromDB) => {
       console.log(allTheBooksFromDB);
-      res.render('books-list', { books: allTheBooksFromDB });
+      res.render("books-list", { books: allTheBooksFromDB });
     })
-    .catch(err =>
+    .catch((err) =>
       console.log(`Err while getting the books from the  DB: ${err}`)
     );
 });
@@ -26,14 +86,14 @@ router.get('/books', (req, res) => {
 // GET route for displaying the book details page
 // ****************************************************************************************
 
-router.get('/books/:someBookId', (req, res) => {
+router.get("/books/:someBookId", (req, res) => {
   const { someBookId } = req.params;
   Book.findById(someBookId)
-    .then(foundBook => {
+    .then((foundBook) => {
       // console.log('Did I find a book?', foundBook);
-      res.render('book-details', foundBook);
+      res.render("book-details", foundBook);
     })
-    .catch(err =>
+    .catch((err) =>
       console.log(`Err while getting the specific book from the  DB: ${err}`)
     );
 });
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const Book = require("../models/Book.model");
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/new-book", (req, res) => res.render("books-create"));
+
+router.get("/modify-book/:someBookId", async (req, res) => {
+  // Selecionar qual o livro a ser atualizado
+  const { someBookId } = req.params;
+
+  try {
+    // Recuperar os dados deste livro
+    const bookData = await Book.findById(someBookId);
+
+    res.render("books-update", {
+      book: bookData,
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+});
 
 module.exports = router;
